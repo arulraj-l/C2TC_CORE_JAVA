503237c4c6a102c1bf2b15f2922aca291d35fcd5
Arul raj <Arul raj@DESKTOP-0PQ9HDN>
2025-11-05 10:32:53 +0530

    day completed

---
+++ C2TC_CORE_JAVA/src/com/tnsif/ifet/day6/usingfinal/FinalMethodClass.java
@@ -0,0 +1,16 @@
+//Program to demonstrate final method 
+package com.tnsif.ifet.day6.usingfinal;
+//class with final method
+class FinalMethodClass {
+	// constructor
+	FinalMethodClass() {
+		System.out.println("This is a default constructor");
+	}
+
+	final int a = 50;
+
+	// Final method
+	final void show() {
+		System.out.println("Value of a: " + a);
+	}
+}
