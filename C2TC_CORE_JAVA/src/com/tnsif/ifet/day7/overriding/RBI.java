2dabbb2b01d946adc30a24b6ba0f153fd91a0d51
Arul raj <Arul raj@DESKTOP-0PQ9HDN>
2025-11-05 11:14:21 +0530

    day7 completed

---
+++ C2TC_CORE_JAVA/src/com/tnsif/ifet/day7/overriding/RBI.java
@@ -0,0 +1,10 @@
+//Program to demonstrate method overriding - Runtime Polymorphism
+
+package com.tnsif.ifet.day7.overriding;
+//Superclass
+public class RBI {
+	public float getRateOfInterest() {
+		return 6.7f;
+	}
+
+}
